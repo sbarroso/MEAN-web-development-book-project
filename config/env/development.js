5bdc51d72bc82d58df4b15ade1c66940858b7284
Santiago Barroso <sbarroso.dev@gmail.com>
2016-01-21 22:51:14 +0100

    Adding passport facebook strategy

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,10 @@
 module.exports = {
 	//Development configuration options
 	db: "mongodb://localhost/mean-book",
-	sessionSecret: "developmentSessionSecret"
+	sessionSecret: "developmentSessionSecret",
+	facebook: {
+		clientId: '',
+		clientSecret: '',
+		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+	}
 }
--- config/passport.js
+++ config/passport.js
@@ -18,4 +18,6 @@ module.exports = function() {
 	});
 
 	require("./strategies/local.js");
+	require("./strategies/facebook.js");
+
 }
